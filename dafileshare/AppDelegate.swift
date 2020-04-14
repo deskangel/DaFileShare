4771585a89ccb72d92f635147e688edaa378848c
William Xue <william.xue@gmail.com>
2020-04-14 18:53:04 +0800

    Initial Commit

---
+++ dafileshare/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dafileshare
+//
+//  Created by William Xue on 4/14/20.
+//  Copyright © 2020 Deskangel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ dafileshare/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dafileshare
+//
+//  Created by William Xue on 4/14/20.
+//  Copyright © 2020 Deskangel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
