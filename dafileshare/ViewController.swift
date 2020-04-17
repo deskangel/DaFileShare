18adba3309829d0ef5e16db79efb7284bc0129a7
William Xue <william.xue@gmail.com>
2020-04-17 17:05:41 +0800

    + process

--- dafileshare/ViewController.swift
+++ dafileshare/ViewController.swift
@@ -9,6 +9,7 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    let task = Process()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +18,16 @@ class ViewController: NSViewController {
         let view = self.view as! DragDestinationView
         view.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidDisappear() {
+        if task.isRunning {
+            task.terminate()
+        }
+       
+        super.viewDidDisappear()
+        
+        print("exit")
     }
 
     override var representedObject: Any? {
@@ -28,9 +39,56 @@ class ViewController: NSViewController {
 
 extension ViewController: FileDragDelegate {
     func didFinishDrag(_ filePath: String) {
-        let url = NSURL(fileURLWithPath: filePath)
+
+        runCommand(filePath: filePath)
+    }
+    
+    func runCommand(filePath: String) {
+        let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
         
-        print(url)
+        taskQueue.async {
+            let appPath = Bundle.main.bundlePath;
+            
+         
+            self.task.launchPath = "\(appPath)/Contents/MacOS/fileweb";
+
+            self.task.arguments = [filePath];
+            
+            self.captureStandarOutput(task: self.task)
+            
+            self.task.launch()
+            self.task.waitUntilExit()
+            
+            let status = self.task.terminationStatus
+            if (status == 0) {
+                print("done!!")
+            } else {
+                print("error")
+            }
+        }
+    }
+    
+    func captureStandarOutput(task: Process) {
+        let outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        
+        outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable,
+                                               object: outputPipe.fileHandleForReading, queue: nil,
+                                               using: {(notification: Notification) in
+                                                
+                                                let output = outputPipe.fileHandleForReading.availableData
+                                                let outputString = String(data: output, encoding: String.Encoding.utf8) ?? ""
+                                                if outputString != "" {
+                                                    DispatchQueue.main.async(execute: {
+                                                        print(">> " + outputString)
+                                                    })
+                                                }
+                                                
+                                                outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+                                                
+        })
     }
 }
 
